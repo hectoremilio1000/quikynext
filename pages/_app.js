ff6bf61c155a46b9cf4479e1c12ccf660c4ffd79
jampierzcode <jampierv127@gmail.com>
2023-07-10 13:04:32 -0500

    modificaciones de variables de entorno

--- pages/_app.js
+++ pages/_app.js
@@ -9,21 +9,35 @@ import "@aws-amplify/ui-react/styles.css";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 const awsConfig = {
-  // Configuración de las variables de entorno
-
-  aws_project_region: process.env.AWS_PROJECT_REGION,
-  aws_appsync_graphqlEndpoint: process.env.AWS_APPSYNC_GRAPHQLENDPOINT,
-  aws_appsync_region: process.env.AWS_APPSYNC_REGION,
-  aws_appsync_authenticationType: process.env.AWS_APPSYNC_AUTHENTICATIONTYPE,
-  aws_cognito_identity_pool_id: process.env.AWS_COGNITO_IDENTITY_POOL_ID,
-  aws_cognito_region: process.env.AWS_COGNITO_REGION,
-  aws_user_pools_id: process.env.AWS_USER_POOLS_ID,
-  aws_user_pools_web_client_id: process.env.AWS_USER_POOLS_WEB_CLIENT_ID,
-  aws_user_files_s3_bucket: process.env.AWS_USER_FILES_S3_BUCKET,
-  aws_user_files_s3_bucket_region: process.env.AWS_USER_FILES_S3_BUCKET_REGION,
+  aws_project_region: process.env.NEXT_PUBLIC_AWS_PROJECT_REGION,
+  aws_appsync_graphqlEndpoint:
+    process.env.NEXT_PUBLIC_AWS_APPSYNC_GRAPHQLENDPOINT,
+  aws_appsync_region: process.env.NEXT_PUBLIC_AWS_APPSYNC_REGION,
+  aws_appsync_authenticationType:
+    process.env.NEXT_PUBLIC_AWS_APPSYNC_AUTHENTICATIONTYPE,
+  aws_cognito_identity_pool_id:
+    process.env.NEXT_PUBLIC_AWS_COGNITO_IDENTITY_POOL_ID,
+  aws_cognito_region: process.env.NEXT_PUBLIC_AWS_COGNITO_REGION,
+  aws_user_pools_id: process.env.NEXT_PUBLIC_AWS_USER_POOLS_ID,
+  aws_user_pools_web_client_id:
+    process.env.NEXT_PUBLIC_AWS_USER_POOLS_WEB_CLIENT_ID,
+  oauth: {},
+  aws_cognito_username_attributes: [],
+  aws_cognito_social_providers: [],
+  aws_cognito_signup_attributes: ["NAME"],
+  aws_cognito_mfa_configuration: "OFF",
+  aws_cognito_mfa_types: ["SMS"],
+  aws_cognito_password_protection_settings: {
+    passwordPolicyMinLength: 8,
+    passwordPolicyCharacters: [],
+  },
+  aws_cognito_verification_mechanisms: ["EMAIL"],
+  aws_user_files_s3_bucket: process.env.NEXT_PUBLIC_AWS_USER_FILES_S3_BUCKET,
+  aws_user_files_s3_bucket_region:
+    process.env.NEXT_PUBLIC_AWS_USER_FILES_S3_BUCKET_REGION,
 };
 
-Amplify.configure({ ...awsConfig, ssr: true });
+Amplify.configure(awsConfig);
 
 export default function MyApp({ Component, pageProps }) {
   return (
