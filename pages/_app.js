7a683e751b79ff69cc95be64dff0ff7ccfc4ca23
hectoremilio1000 <hectoremilio1000@gmail.com>
2023-07-31 20:05:09 -0600

    cambios 3

--- pages/_app.js
+++ pages/_app.js
@@ -44,7 +44,7 @@ export default function MyApp({ Component, pageProps }) {
     <>
       <LayoutFinal>
         <Component {...pageProps} />
-        <ToastContainer
+        <ToastContainer 
           position="top-right"
           autoClose={2000}
           hideProgressBar={false}
